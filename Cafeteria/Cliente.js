05ceb23dee4c9886c84c8b88ccdc3db4d91f8fb9
Francisco Joel Olvera Sánchez <125052657@upq.edu.mx>
2026-09-18 20:16:20 -0600

    Avance individual

---
+++ Cafeteria/Cliente.js
@@ -0,0 +1,53 @@
+const productos = [
+  { id: 1, nombre: "Taco", precio: 25 },
+  { id: 2, nombre: "Refresco", precio: 20 },
+  { id: 3, nombre: "Quesadilla", precio: 35 },
+  { id: 4, nombre: "Pan dulce", precio: 10}
+];
+
+const pedidos = [];
+
+function agregarPedido(producto) {
+  pedidos.push(producto);
+}
+
+function mostrarMenu() {
+  const menu = document.getElementById("menu");
+
+  productos.forEach(p => {
+    menu.innerHTML += `
+      <li>
+        <span>${p.nombre} - $${p.precio}</span>
+        <button onclick="crearPedido(${p.id})">Agregar</button>
+      </li>
+    `;
+  });
+}
+
+function crearPedido(id) {
+  const producto = productos.find(p => p.id === id);
+  agregarPedido(producto);
+  listarPedidos();
+}
+
+function listarPedidos() {
+  const lista = document.getElementById("lista-ticket");
+  const total = document.getElementById("total");
+  let suma = 0;
+
+  lista.innerHTML = "";
+
+  if (pedidos.length === 0) {
+    lista.innerHTML = "<li>Aún no has agregado nada</li>";
+  }
+
+  pedidos.forEach(p => {
+    lista.innerHTML += `<li><span>${p.nombre}</span><span>$${p.precio}</span></li>`;
+    suma = suma + p.precio;
+  });
+
+  total.textContent = `$${suma}`;
+}
+
+mostrarMenu();
+listarPedidos();
